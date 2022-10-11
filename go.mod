a69ce392f754101eccdf770209bf8f940deb74c0
Brandon Wagner <bmwagner10@gmail.com>
2022-10-10 23:22:13 -0500

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/bwagner5/gh-relative-dates
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+	"time"
+)
+
+var dateRegex = regexp.MustCompile(`(created|closed):[<>]([0-9+dmh]+)`)
+
+// Get env var or default
+func getEnv(key, fallback string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+	return fallback
+}
+
+// Get the port to listen on
+func getListenAddress() string {
+	port := getEnv("PORT", "1338")
+	return ":" + port
+}
+
+// Log the env variables required for a reverse proxy
+func logSetup() {
+	log.Printf("Server will run on: %s\n", getListenAddress())
+}
+
+// Serve a reverse proxy for a given url
+func serveReverseProxy(target string, res http.ResponseWriter, req *http.Request) {
+	// parse the url
+	targetURL, _ := url.Parse(target)
+	log.Printf("redirecting to %s", req.URL)
+	qvals, err := url.ParseQuery(req.URL.RawQuery)
+	if err != nil {
+		log.Printf("unable to unescape query string %s: %v", req.URL.RawQuery, err)
+	} else if qvals.Get("q") != "" {
+		decodedValue := qvals.Get("q")
+		submatches := dateRegex.FindStringSubmatch(decodedValue)
+		if len(submatches) > 2 {
+			for _, candidateDuration := range submatches[2:] {
+				ogDuration := candidateDuration
+				if strings.HasSuffix(candidateDuration, "d") {
+					strippedDuration := strings.ReplaceAll(candidateDuration, "d", "")
+					iDur, err := strconv.Atoi(strippedDuration)
+					if err != nil {
+						log.Printf("unable to convert duration %s to int: %v", candidateDuration, err)
+						continue
+					}
+					candidateDuration = fmt.Sprintf("%dh", iDur*24)
+				}
+				duration, err := time.ParseDuration(candidateDuration)
+				if err != nil {
+					log.Printf("unable to parse duration %s", candidateDuration)
+					continue
+				}
+				decodedValue = strings.ReplaceAll(decodedValue, ogDuration, time.Now().Add(-1*duration).Format("2006-01-02"))
+				log.Printf("rewrote query string to %s", decodedValue)
+			}
+			qvals.Set("q", decodedValue)
+			req.URL.RawQuery = qvals.Encode()
+		}
+	}
+
+	// create the reverse proxy
+	proxy := httputil.NewSingleHostReverseProxy(targetURL)
+
+	// Update the headers to allow for SSL redirection
+	req.URL.Host = targetURL.Host
+	req.URL.Scheme = targetURL.Scheme
+	req.Host = targetURL.Host
+
+	log.Printf("redirecting rewritten req to %s%s", target, req.URL)
+	// Note that ServeHttp is non blocking and uses a go routine under the hood
+	proxy.ServeHTTP(res, req)
+}
+
+func handleRequestAndRedirect(res http.ResponseWriter, req *http.Request) {
+	serveReverseProxy("https://github.com", res, req)
+}
+
+func main() {
+	logSetup()
+
+	// start server
+	http.HandleFunc("/", handleRequestAndRedirect)
+	if err := http.ListenAndServe(getListenAddress(), nil); err != nil {
+		panic(err)
+	}
+}
